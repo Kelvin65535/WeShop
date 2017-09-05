1c00c77b2ee2f5ddb2a8040f9d8f6e34ea117da3
YeJiayong <kelvin65535@gmail.com>
2017-09-05 22:38:13 +0800

    上传/extend文件夹

---
+++ extend/qcloudcos/include.php
@@ -0,0 +1,12 @@
+<?php
+
+//require_once(__DIR__ . DIRECTORY_SEPARATOR . 'qcloudcos' . DIRECTORY_SEPARATOR . 'auth.php');
+require_once(__DIR__ . 'auth.php');
+//require_once(__DIR__ . DIRECTORY_SEPARATOR . 'qcloudcos' . DIRECTORY_SEPARATOR . 'conf.php');
+require_once(__DIR__ . 'conf.php');
+//require_once(__DIR__ . DIRECTORY_SEPARATOR . 'qcloudcos' . DIRECTORY_SEPARATOR . 'cosapi.php');
+require_once(__DIR__ . 'cosapi.php');
+//require_once(__DIR__ . DIRECTORY_SEPARATOR . 'qcloudcos' . DIRECTORY_SEPARATOR . 'http_client.php');
+require_once(__DIR__ . 'http_client.php');
+//require_once(__DIR__ . DIRECTORY_SEPARATOR . 'qcloudcos' . DIRECTORY_SEPARATOR . 'slice_uploading.php');
+require_once(__DIR__ . 'slice_uploading.php');
---
+++ extend/test/test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kelvin
+ * Date: 2017/1/25
+ * Time: 下午10:19
+ */
+
+namespace test;
+
+class test{
+    public function fuck(){
+        echo "fuck";
+    }
+}
