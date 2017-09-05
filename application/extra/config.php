9bd54a972821d80b9e3a235891f74de7e814ee79
YeJiayong <kelvin65535@gmail.com>
2017-09-05 23:20:20 +0800

    修改访问配置

--- application/extra/config.php
+++ application/extra/config.php
@@ -25,7 +25,7 @@ return [
 
     //商城配置
     //商城根目录(记得使用完整的域名) 示例：http://baidu.com
-    'docroot' => 'http://shop.hakaei.iego.cn/',
+    'docroot' => 'http://shop.hakaei.iego.cn/weshop/',
 
     //商城名称
     'shopname' => '微信商城',
